629d34314f66b46a8aa527b152f329bdc48e9abf
nguyenmanhdung128 <nguyenmanhdung128@gmail.com>
2023-04-23 23:31:06 +0700

    commit baitaptrenlop01 bai 01 02

---
+++ baitaptrenlop01/streaming_server.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <time.h>
+
+int main() 
+{
+    int listener = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (listener == -1)
+    {
+        perror("socket() failed");
+        return 1;
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(9090);
+
+    if (bind(listener, (struct sockaddr *)&addr, sizeof(addr))) 
+    {
+        perror("bind() failed");
+        return 1;
+    }
+
+    if (listen(listener, 5)) 
+    {
+        perror("listen() failed");
+        return 1;
+    }
+
+    struct sockaddr_in clientAddr;
+    int clientAddrLen = sizeof(clientAddr);
+
+    int client = accept(listener, (struct sockaddr *)&clientAddr, &clientAddrLen);
+    if (client == -1)
+    {
+        perror("accept() failed");
+        return 1;
+    }
+    printf("New client connected: %d\n", client);
+
+    // Truyen nhan du lieu
+    char buf[256];
+    
+
+
+    char fbuf[512];
+
+    while (1)
+    {
+        int ret = recv(client, buf, sizeof(buf), 0);
+        if (ret <= 0)
+            break;
+
+        buf[ret] = 0;
+
+        int count = 0;
+        char *ret2;
+        const char needle[11] = "0123456789";
+        ret2 = strstr(buf, needle);
+
+        while (ret2 !=NULL)
+        {
+            count++;
+            memcpy(ret2, ret2+3, strlen(ret2)+1-3);
+            ret2 = strstr (ret2, needle);
+        }
+        
+        
+        printf("so luong chuoi 0123456789 la: %d\n",count);
+
+    }
+    close(client);
+    close(listener);    
+
+    return 0;
+}
