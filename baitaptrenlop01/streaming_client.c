a1afef418e27b7a0421abaef1e31ce5a9590b560
nguyenmanhdung128 <nguyenmanhdung128@gmail.com>
2023-04-24 09:37:40 +0700

    commit baitaptrenlop01 bai 02

--- baitaptrenlop01/streaming_client.c
+++ baitaptrenlop01/streaming_client.c
@@ -18,54 +18,6 @@ int main() {
         perror("connect() failed");
         return 1;
     }
-        
-    // // Truyen nhan du lieu
-    // char buf[256];
-    // char computer_name[64];
-
-    // // Nhap ten may tinh
-    // printf("Nhap ten may tinh: ");
-    // scanf("%s", computer_name);
-
-    // // Xoa \n trong bo dem
-    // getchar();
-
-    // // Chuyen vao buffer
-    // strcpy(buf, computer_name);
-    // int pos = strlen(computer_name);
-    // buf[pos] = 0;
-    // pos++;
-
-    // int n;
-    // printf("Nhap so luong o dia:\n");
-    // scanf("%d", &n);
-
-    // // Xoa \n trong bo dem
-    // getchar();
-
-    // char disk_letter;
-    // short int disk_size;
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     printf("Nhap ten o dia: ");
-    //     scanf("%c", &disk_letter);
-        
-    //     printf("Nhap kich thuoc o dia: ");
-    //     scanf("%hd", &disk_size);
-        
-    //     // Xoa \n trong bo dem
-    //     getchar();
-        
-    //     // Chuyen vao buffer
-    //     buf[pos] = disk_letter;
-    //     pos++;
-    //     memcpy(buf + pos, &disk_size, sizeof(disk_size));
-    //     pos += sizeof(disk_size);
-    // }
-
-    // printf("Buffer size: %d\n", pos);
-    // send(client, buf, pos, 0);
 
     char data[156];
     FILE *f = fopen("streaming.txt", "a+");
